859daef62decbc6e2a94da4550d50102c5858548
Javiera Mutis <122648544+javimutis@users.noreply.github.com>
2025-03-26 14:53:11 -0300

    XML de App Settings listo Implementación de DataStore, guardar datos
    Falta recuperar datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.datastore.preferences.core.jvm)
+    implementation(libs.androidx.datastore.core.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -54,7 +56,10 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
 
-//Picasso
+    //Picasso
     implementation(libs.picasso)
+
+    //DataStore
+    implementation(libs.datastore)
 
 }
--- app/src/main/java/com/javimutis/androidmaster/settings/SettingsActivity.kt
+++ app/src/main/java/com/javimutis/androidmaster/settings/SettingsActivity.kt
@@ -1,21 +1,91 @@
 package com.javimutis.androidmaster.settings
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.javimutis.androidmaster.R
+import com.javimutis.androidmaster.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+//Persistencia de datos. Guardar información. Preferencias en este caso. Se puede usar room para guardar en la base de datos
+// SQLlite para guardar en la base de datos y en este caso se va a usar DataStore para guardar en la base de datos.
+// Lo ideal es que esté en un fichero distinto pero para empezar está bien que esté aquí
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class SettingsActivity : AppCompatActivity() {
+
+    companion object {
+        const val VOLUME_LVL = "volume_lvl"
+      const val KEY_BLUETOOTH = "key_bluetooth"
+      const val KEY_VIBRATION = "key_vibration"
+      const val KEY_DARK_MODE = "key_dark_mode"
+    }
+
+    private lateinit var binding: ActivitySettingsBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initUI()
+
+    }
+
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener { _, value, _ ->
+            Log.i("mutisApp", "Volumen $value")
+            CoroutineScope(Dispatchers.IO).launch {
+                saveVolume(value.toInt())
+            }
+        }
+        binding.switchBluetooth.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_BLUETOOTH, value)
+            }
+        }
+        binding.switchVibration.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_VIBRATION, value)
+            }
+        }
+        binding.switchDarkMode.setOnCheckedChangeListener { _, value ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_DARK_MODE, value)
+            }
+        }
+    }
+
+    private suspend fun saveVolume(value: Int) {
+        dataStore.edit { preference ->
+            preference[intPreferencesKey(VOLUME_LVL)] = value
+        }
+    }
+
+    private suspend fun saveOptions(key: String, value: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[booleanPreferencesKey(key)] = value
         }
     }
 }
