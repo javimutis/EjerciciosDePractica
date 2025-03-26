289dec2a2f89d1b75653dac71e4498afdfdb70d3
Javiera Mutis <122648544+javimutis@users.noreply.github.com>
2025-03-26 18:29:25 -0300

    Ejercicio de practica de Clases POO comentado

---
+++ app/src/main/java/com/javimutis/androidmaster/exercises/classes/Exercise1Classes.kt
@@ -0,0 +1,20 @@
+package com.javimutis.androidmaster.exercises.classes
+
+/*Crea una clase que pueda representar la estructura de una canción. La clase Song debe incluir estos elementos de código:
+
+Propiedades para el título, el artista, el año de publicación y el recuento de reproducciones
+Propiedad que indica si la canción es popular (si el recuento de reproducciones es inferior a 1,000, considera que es poco popular)
+Un método para imprimir la descripción de una canción en este formato:
+"[Título], interpretada por [artista], se lanzó en [año de lanzamiento]".*/
+
+// Función principal para probar la clase Song
+fun main() {
+    // Creamos una instancia de la clase Song
+    val song = Song("Bohemian Rhapsody", "Queen", 1975, 1000)
+
+    // Llamamos al método para imprimir la descripción
+    song.printDescription()
+
+    // Verificamos si la canción es popular
+    println("¿Es popular? ${song.isPopular}")
+}
